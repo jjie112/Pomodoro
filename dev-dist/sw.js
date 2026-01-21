af8f394edee6fc23bd70a955fb1ab5ea5fe060f9
jjie112 <windser54@gmail.com>
2026-01-21 09:09:46 +0800

    base路徑更換成Pomodoro

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-7a5e81cd'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.38p6e2oc2e"
+    "revision": "0.lpm5otvuob"
   }], {
     "ignoreURLParametersMatching": [/.*/]
   });
